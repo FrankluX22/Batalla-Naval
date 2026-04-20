2522c0bfda683f8ea98013d9d0f2964aff87046d
FrankluX22 <francoch22@outlook.com>
2026-04-20 01:28:49 -0300

    Creacion del Main

---
+++ Main.java
@@ -0,0 +1,37 @@
+/**
+ * Batalla Naval - Aplicación Principal
+ * Juego de Batalla Naval en Java
+ */
+
+public class Main {
+    
+    public static void main(String[] args) {
+        System.out.println("=== BIENVENIDO A BATALLA NAVAL ===");
+        System.out.println();
+        
+        // Inicializar el juego
+        iniciarJuego();
+    }
+    
+    /**
+     * Inicia el flujo principal del juego
+     */
+    private static void iniciarJuego() {
+        System.out.println("Iniciando nueva partida...");
+        System.out.println();
+        
+        // TODO: Crear tableros para ambos jugadores
+        System.out.println("Creando tableros...");
+        
+        // TODO: Posicionar barcos
+        System.out.println("Posicionando barcos...");
+        
+        // TODO: Iniciar bucle principal del juego
+        System.out.println("¡Que comience la batalla!");
+        
+        // TODO: Implementar lógica de turnos
+        // TODO: Implementar sistema de disparos
+        // TODO: Implementar detección de hundimiento
+        // TODO: Implementar condiciones de victoria
+    }
+}
