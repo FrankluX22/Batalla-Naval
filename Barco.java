5b6b7f26f1f91bb61471b9f4e27812cf6d61b7cb
FrankluX22 <francoch22@outlook.com>
2026-04-22 01:50:20 -0300

    Creacion de la Clase Barco

---
+++ Barco.java
@@ -0,0 +1,32 @@
+public class Barco {
+    private String nombre;
+    private int tamanio;
+    private int impactos;
+
+    // Constructor
+    public Barco(String nombre, int tamanio) {
+        this.nombre = nombre;
+        this.tamanio = tamanio;
+        this.impactos = 0;
+    }
+    // Método para registrar un impacto en el barco
+    public void recibirImpacto() {
+        impactos++;
+    }
+    // Método para verificar si el barco está hundido
+    public boolean estaHundido() {
+        return impactos >= tamanio;
+    }
+
+    // Getters
+    public String getNombre() {
+        return nombre;
+    }
+    public int getTamanio() {
+        return tamanio;
+    }
+    public int getImpactos() {
+        return impactos;
+    }
+    
+}
